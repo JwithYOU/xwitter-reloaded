9c9898b739e1c744d138521533858c13525a317f
J with YOU <khy960101@gmail.com>
2023-11-10 13:38:04 +0900

    firebase 재연결

---
+++ src/components/timeline.tsx
@@ -0,0 +1,80 @@
+import {
+  collection,
+  /*  getDocs, */
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { db } from "../firebase";
+import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
+
+export interface ITweet {
+  id: string;
+  photo?: string;
+  xweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+`;
+
+export default function Timeline() {
+  const [xweets, setXweet] = useState<ITweet[]>([]);
+
+  useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "xweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+      /* const spanshot = await getDocs(tweetsQuery);
+        const tweets = spanshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        }); */
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const xweets = snapshot.docs.map((doc) => {
+          const { xweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            xweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setXweet(xweets);
+      });
+    };
+    fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
+  }, []);
+  return (
+    <Wrapper>
+      {xweets.map((xweet) => (
+        <Tweet key={xweet.id} {...xweet} />
+      ))}
+    </Wrapper>
+  );
+}
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,15 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
-  authDomain: import.meta.env.VITE_REACT_APP_AUTH_ID,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_ID,
-  messagingSenderId: import.meta.env.VITE_MESSAGE_ID,
-  appId: import.meta.env.VITE__APP_ID,
+  apiKey: "AIzaSyAC7afcaTbYWuQt3xTOqMS8f7BZtXq0JWI",
+  authDomain: "xwitter-reload.firebaseapp.com",
+  projectId: "xwitter-reload",
+  storageBucket: "xwitter-reload.appspot.com",
+  messagingSenderId: "692959651601",
+  appId: "1:692959651601:web:8731b360c33854904e8eb0",
 };
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+//   authDomain: import.meta.env.VITE_REACT_APP_AUTH_ID,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_ID,
+//   messagingSenderId: import.meta.env.VITE_MESSAGE_ID,
+//   appId: import.meta.env.VITE__APP_ID,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
